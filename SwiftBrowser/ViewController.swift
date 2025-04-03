c6285a9b821d8f8342eb7758d74ac29936c14c86
brycebyington <legobrick222@gmail.com>
2025-04-03 14:25:43 -0400

    Basic CoreText setup

--- SwiftBrowser/ViewController.swift
+++ SwiftBrowser/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import Cocoa
+import AppKit
+import CoreText
+
+final class LabelView: NSView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ dirtyRect: CGRect) {
+        super.draw(dirtyRect)
+        
+        guard let context = NSGraphicsContext.current?.cgContext else { return }
+        
+        let text = "Hello, World!"
+        let attributedString = NSAttributedString(string: text)
+        let ctLine = CTLineCreateWithAttributedString(attributedString)
+        
+        context.textPosition = CGPoint(x: 0, y: bounds.height)
+        CTLineDraw(ctLine, context)
+        
+    }
+}
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        super.view.addSubview(LabelView())
+        
         // Do any additional setup after loading the view.
     }
 
