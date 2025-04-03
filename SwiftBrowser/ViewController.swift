bd61d3c8bf582b2fc2e362823434b32dc7fe9439
brycebyington <legobrick222@gmail.com>
2025-04-03 11:18:00 -0400

    Initial Commit

---
+++ SwiftBrowser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftBrowser
+//
+//  Created by Bryce Byington on 4/3/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
